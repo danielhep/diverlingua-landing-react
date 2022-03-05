755dd54e0af9c51909c79a490b83520dd10c90dc
Daniel Heppner <dh@danielheppner.com>
2022-03-04 21:00:25 -0800

    round photos

--- components/Employees.js
+++ components/Employees.js
@@ -46,7 +46,7 @@ export default function Employees () {
       </div>
       <style global jsx>{`
         .img-wrapper span {
-          border-radius: 0.75rem;
+          border-radius: 9999px;
           box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
         }
       `}
